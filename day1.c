7fd83703410e17b9b6118f9358fd8815760b88f9
riajoshi13 <Ria.26070@stu.upes.ac.in>
2025-08-28 09:38:43 +0530

    Added Day 1: Q1 (Sum) and Q2 (Basic Arithmetic)

--- day1.c
+++ day1.c
@@ -1,10 +1,39 @@
 #include <stdio.h>
 
-int main ()  {
-    int num1, num2, sum;
-    scanf("%d", &num1);
-    scanf("%d", &num2);
-    sum = num1 + num2;
-    printf("%d\n", sum);
+// Q1: Sum of two numbers
+void q1() {
+    int a, b, sum;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    sum = a + b;
+    printf("Sum = %d\n", sum);
+}
+
+// Q2: Sum, Difference, Product, Quotient
+void q2() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("Sum = %d\n", a + b);
+    printf("Difference = %d\n", a - b);
+    printf("Product = %d\n", a * b);
+
+    if (b != 0) {
+        printf("Quotient = %d\n", a / b);
+    } else {
+        printf("Division by zero not allowed\n");
+    }
+}
+
+int main() {
+    printf("Running Q1:\n");
+    q1();
+
+    printf("\nRunning Q2:\n");
+    q2();
+
     return 0;
 }
