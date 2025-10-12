eb190f95ca632a1ae2dac8f70787c85e0ae38ce4
riajoshi13 <Ria.26070@stu.upes.ac.in>
2025-10-12 12:38:56 +0530

    Added Day 51: Hello World program from Hackerrank

---
+++ day51.c
@@ -0,0 +1,23 @@
+// Day 51
+// Q1: Print Hello, World!
+// Hackerrank Link: https://www.hackerrank.com/challenges/hello-world-c/problem?isFullScreen=true
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Hello, World!\n");  // prints Hello, World!
+    return 0;
+}
+
+/*
+Sample Test Case:
+
+Input:
+(no input)
+
+Output:
+Hello, World!
+*/
