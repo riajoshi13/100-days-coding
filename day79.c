7067891bfdb8f5b194c8cb9efe57d368aac1918e
riajoshi13 <Ria.26070@stu.upes.ac.in>
2025-11-27 00:38:31 +0530

    Day 79: Sum and average from numbers file

---
+++ day79.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    FILE *fp;
+    int num, sum = 0, count = 0;
+    float avg;
+
+    fp = fopen("numbers.txt", "r");
+    if (fp == NULL) {
+        printf("Error: numbers.txt not found!\n");
+        return 1;
+    }
+
+    while (fscanf(fp, "%d", &num) == 1) {
+        sum += num;
+        count++;
+    }
+
+    fclose(fp);
+
+    if (count == 0) {
+        printf("File contains no numbers!\n");
+        return 1;
+    }
+
+    avg = (float) sum / count;
+
+    printf("Sum = %d\n", sum);
+    printf("Average = %.2f\n", avg);
+
+    return 0;
+}
