fe244823cbf1a129791cda43ca92fa3ec6b1f0c2
riajoshi13 <Ria.26070@stu.upes.ac.in>
2025-11-27 15:45:19 +0530

    Day 91: Define Student struct with name, roll_no, and marks; read and print student data

---
+++ day91.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int roll_no;
+    float marks;
+};
+
+int main() {
+    struct Student s;
+
+    printf("Enter name: ");
+    scanf("%s", s.name);
+
+    printf("Enter roll number: ");
+    scanf("%d", &s.roll_no);
+
+    printf("Enter marks: ");
+    scanf("%f", &s.marks);
+
+    printf("Name: %s | Roll: %d | Marks: %.0f\n", s.name, s.roll_no, s.marks);
+
+    return 0;
+}
