9e7267fb11b6ab22af08600b2e7fd56be68f8edd
riajoshi13 <Ria.26070@stu.upes.ac.in>
2025-11-27 15:42:39 +0530

    Day 89: Show enums store integers by printing assigned values

---
+++ day89.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+enum Status { SUCCESS, FAILURE, TIMEOUT };
+
+int main() {
+    printf("SUCCESS=%d, FAILURE=%d, TIMEOUT=%d\n",
+           SUCCESS, FAILURE, TIMEOUT);
+    return 0;
+}
