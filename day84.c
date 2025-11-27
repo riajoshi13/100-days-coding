4f95edb0fc585c6dae22da87ca9bbf6e5078d6c2
riajoshi13 <Ria.26070@stu.upes.ac.in>
2025-11-27 15:35:29 +0530

    Day 84: Enum with SUCCESS, FAILURE, and TIMEOUT; print messages accordingly

---
+++ day84.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+enum Status { SUCCESS, FAILURE, TIMEOUT };
+
+int main() {
+    char input[20];
+    scanf("%s", input);
+
+    enum Status s;
+
+    if (strcmp(input, "SUCCESS") == 0)
+        s = SUCCESS;
+    else if (strcmp(input, "FAILURE") == 0)
+        s = FAILURE;
+    else
+        s = TIMEOUT;
+
+    switch (s) {
+        case SUCCESS:
+            printf("Operation successful");
+            break;
+        case FAILURE:
+            printf("Operation failed");
+            break;
+        case TIMEOUT:
+            printf("Operation timed out");
+            break;
+    }
+
+    return 0;
+}
